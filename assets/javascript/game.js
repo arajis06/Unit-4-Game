75232a4b371cb22c1fda0eeac0f67e7d73feb4f9
ALEXIS RAJIS <arajis06@gmail.com>
2019-05-16 14:11:18 -0400

    fixed crystal sum

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,15 +8,13 @@ var sumOfCrystalsNum = 0;
 //FUNCTION(FOR STARTING AND RESETING THE GAME FOR EVERY ROUND)===================================
 var startGame = function() {
 
-    $(".game-crystals").empty();
+    $(".game-crystals").empty();   //EMPTY THE NUMBERS IN CRYSTALS DIVS TO BE REPLACED WITH NEW RANDOM NUMBERS.
 
-    sumOfCrystalsNum = 0;
 
     // GENERATING THE GAME RANDOM NUMBERS (*subtracting 19 from 120 to get 101, is so that the random numbers stays in the range between 19-120 since using Math.random starts counting from an index of 0)
     game_RandomNum = Math.floor(Math.random() * 101) + 19; 
 
     $("#game-random-number").html('Game Generated Number: ' + game_RandomNum);  //ADDING NUMBER TO THE INDEX.HTML FILE
-        // console.log(game_RandomNum);
 
     // CREATING A FOR-LOOP TO RUN THE SAME CODE OVER & OVER AGAIN FOR THE 4 CRYSTALS WITH DIFF VALUES
     for(var i = 0; i < 4; i++) {
@@ -32,30 +30,41 @@ var startGame = function() {
                 "crystal-hidden-number": crystal_RandomNum //ADDING THE RANDOM NUMBERS TO EACH OF THE 4 CRYSTALS
             });
             
-            crystal.html(crystal_RandomNum);  //SHOWS RANDOM NUMBER IN CRYSTAL BOXES  --TESTING GAME ONLY--
+            crystal.html(crystal_RandomNum);  //SHOWS IN THE DOM THE RANDOM NUMBER IN THE CRYSTAL BOXES  --TESTING GAME ONLY-- DELETE
 
         $(".game-crystals").append(crystal);   //THIS WILL APPENDING THE 4 NEW CRYSTAL DIVS TO THE INDEX.HTML FILE 
-            //console.log("crystal");
-        }
-    
+    }
+
+    $("#crystals-sum").html(sumOfCrystalsNum); //THIS WILL PASS THE TOTAL SUM OF ALL THE CRYSTALS THAT WERE CLICKED IN THE INDEX.HTML FILE
+
 }
+//==============================================================================================
 
 //MAIN GAME=====================================================================================
-startGame();
+
+startGame(); //RUN EVERY TIME THE PAGE LOADS
+
 //FUNCTION(FOR ON CLICK)=========================================================================
+
 //ADDING AN EVENT LISTENER OF "ON.CLICK" SO THAT ONCE CLICKED THE HIDDEN NUMBERS DISPLAY.
-$(document).on("click", ".crystal", function() { 
+//$(".crystal").on("click", function() {            // !!!!not working now- figure out why. 
+$(document).on("click", ".crystal", function() {    //!!!!this works bc of EVEN DELEGATION RULES)
 
     var hiddenNums = parseInt($(this).attr('crystal-hidden-number')); //ANALYZES OBJECT(HIDDENNUMS) STRING AND RETURNS AN INTERGER(WHOLE NUMBER)
 
-    sumOfCrystalsNum += hiddenNums;  //THIS IS ADDING THE PREVIOUS UNLOCKED NUMBERS AND THE NEW UNLOCKED CRYSTAL HIDDEN NUMBER TOGETHER FOR SU
-        console.log(sumOfCrystalsNum)
+    sumOfCrystalsNum += hiddenNums;  //THIS IS WILL ADD THE PREVIOUS UNLOCKED NUMBERS AND THE NEW UNLOCKED CRYSTAL HIDDEN NUMBER TOGETHER FOR SUM
+
+    $("#crystals-sum").html(sumOfCrystalsNum); //THIS WILL PASS THE TOTAL SUM OF ALL THE CRYSTALS THAT WERE CLICKED IN THE INDEX.HTML FILE
+
+    console.log(sumOfCrystalsNum)
 
     //CONDITIONALS OF THE FUNCTION CALLED=========================================================
     if(sumOfCrystalsNum > game_RandomNum) {  //IF THE SUM OF ALL CLICKED CRYSTAL NUMBERS ARE GREATER THAN THE GAME RANDOM NUMBER .....
         losses++; //INCREMENT THE LOSE-COUNTER
         alert("YOU LOSE!");
-        $(".lose-counter").html(losses)   //...ADD TO LOSE-COUNTER IN THE INDEX.HTML FILE
+        $(".lose-counter").html(losses);   //...PASS TO LOSE-COUNTER IN THE INDEX.HTML FILE
+
+        sumOfCrystalsNum = 0; //RESETS THE TOTAL OF HIDDEN CRYSTALS THAT WERE UNLOCKED BY THE CLICK FOR NEW GAME
 
         startGame(); //RESETS THE GAME AGAIN AFTER LOST
 
@@ -63,7 +72,9 @@ $(document).on("click", ".crystal", function() {
     else if(sumOfCrystalsNum === game_RandomNum) {   //BUT IF THE SUM OF ALL CLICKED CRYSTAL NUMBERS EQUAL THE GAME RANDOM NUMBER...
         wins++;  //INCREMENT THE WIN-COUNTER
         alert("YOU WIN!");
-        $(".win-counter").html(wins) //...ADD TO WIN COUNTER IN THE INDEX.HTML FILE
+        $(".win-counter").html(wins); //...PASS TO WIN-COUNTER IN THE INDEX.HTML FILE
+
+        sumOfCrystalsNum = 0; //RESETS THE TOTAL OF HIDDEN CRYSTALS THAT WERE UNLOCKED BY THE CLICK FOR NEW GAME
 
         startGame(); //RESETS THE GAME AGAIN AFTER WIN
 
