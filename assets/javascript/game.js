6a5702cc249b2c14514addeb3d9774b7ca6e6fbf
ALEXIS RAJIS <arajis06@gmail.com>
2019-05-16 12:39:49 -0400

    working code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,56 +2,73 @@
 var game_RandomNum;
 var losses = 0;
 var wins = 0;
-var unlockedNum = 0;
+var sumOfCrystalsNum = 0;
 
 
+//FUNCTION(FOR STARTING AND RESETING THE GAME FOR EVERY ROUND)===================================
+var startGame = function() {
 
-// GENERATES GAME RANDOM NUMBERS (subtract 19 from 120 to get 101 so that the random numbers stay in the range between 19-120 since using Math.random starts counting from 0)
-game_RandomNum = Math.floor(Math.random() * 101) + 19; 
-$("#game-random-number").html('Game Generated Number: ' + game_RandomNum);
-// console.log(game_RandomNum);
+    $(".game-crystals").empty();
 
-// CREATING A FOR-LOOP TO RUN THE SAME CODE OVER & OVER AGAIN FOR THE 4 CRYSTALS WITH DIFF VALUES
-for(var i = 0; i < 4; i++) {
-    var crystal_RandomNum = Math.floor(Math.random() * 11) + 1; // GENERATES CRYSTAL RANDOM NUMBERS 1-12
-     //console.log(crystal_RandomNum);
+    sumOfCrystalsNum = 0;
 
-    var crystal = $("<div>");   //THE CRYSTAL VARIABLE IS ASSIGNED A VALUE OF <DIV> SO THAT IT CAN BE USED TO INSIDE THE CRYSTALS TO CREATE 4 NESTED ELEMENTS 
+    // GENERATING THE GAME RANDOM NUMBERS (*subtracting 19 from 120 to get 101, is so that the random numbers stays in the range between 19-120 since using Math.random starts counting from an index of 0)
+    game_RandomNum = Math.floor(Math.random() * 101) + 19; 
 
-        //BELOW, THE OBJECT(CRYSTAL) IS BEING SET WITH ATTR() TO ATTACH MULTIPLE ATTRIBUTE AND VALUES TO BE APPLIED TO THE OBJECT AS PROPERTIES OF THE CRYSTAL
-        crystal.attr({ 
-            "class": 'crystal', //CREATING A CLASS FOR THE OBJECT(CRYSTAL) WHICH CAN BE USED FOR STYLING IN CSS 
-            "crystal-hidden-number": crystal_RandomNum //ADDING THE RANDOM NUMBERS TO EACH OF THE 4 CRYSTALS
-        });  
+    $("#game-random-number").html('Game Generated Number: ' + game_RandomNum);  //ADDING NUMBER TO THE INDEX.HTML FILE
+        // console.log(game_RandomNum);
 
-    $(".game-crystals").append(crystal);   //THIS WILL APPENDING THE 4 NEW CRYSTAL DIVS TO THE INDEX.HTML FILE 
-      //console.log("crystal");
+    // CREATING A FOR-LOOP TO RUN THE SAME CODE OVER & OVER AGAIN FOR THE 4 CRYSTALS WITH DIFF VALUES
+    for(var i = 0; i < 4; i++) {
+
+        var crystal_RandomNum = Math.floor(Math.random() * 11) + 1; // GENERATES CRYSTAL RANDOM NUMBERS 1-12
+            //console.log(crystal_RandomNum);
+
+        var crystal = $("<div>");   //THE CRYSTAL VARIABLE IS ASSIGNED A VALUE OF <DIV> SO THAT IT CAN BE USED TO INSIDE THE CRYSTALS TO CREATE 4 NESTED ELEMENTS 
+
+            //BELOW, THE OBJECT(CRYSTAL) IS BEING SET WITH ATTR() TO ATTACH MULTIPLE ATTRIBUTE AND VALUES TO BE APPLIED TO THE OBJECT AS PROPERTIES OF THE CRYSTAL
+            crystal.attr({ 
+                "class": 'crystal', //CREATING A CLASS FOR THE OBJECT(CRYSTAL) WHICH CAN BE USED FOR STYLING IN CSS 
+                "crystal-hidden-number": crystal_RandomNum //ADDING THE RANDOM NUMBERS TO EACH OF THE 4 CRYSTALS
+            });
+            
+            crystal.html(crystal_RandomNum);  //SHOWS RANDOM NUMBER IN CRYSTAL BOXES  --TESTING GAME ONLY--
+
+        $(".game-crystals").append(crystal);   //THIS WILL APPENDING THE 4 NEW CRYSTAL DIVS TO THE INDEX.HTML FILE 
+            //console.log("crystal");
+        }
+    
 }
 
-
+//MAIN GAME=====================================================================================
+startGame();
+//FUNCTION(FOR ON CLICK)=========================================================================
 //ADDING AN EVENT LISTENER OF "ON.CLICK" SO THAT ONCE CLICKED THE HIDDEN NUMBERS DISPLAY.
-$(".crystal").on("click", function() { 
+$(document).on("click", ".crystal", function() { 
 
     var hiddenNums = parseInt($(this).attr('crystal-hidden-number')); //ANALYZES OBJECT(HIDDENNUMS) STRING AND RETURNS AN INTERGER(WHOLE NUMBER)
 
-    unlockedNum += hiddenNums;  //THIS IS ADDING THE PREVIOUS UNLOCKED NUMBERS AND THE NEW UNLOCKED CRYSTAL HIDDEN NUMBER TOGETHER FOR SU
-    console.log(unlockedNum)
+    sumOfCrystalsNum += hiddenNums;  //THIS IS ADDING THE PREVIOUS UNLOCKED NUMBERS AND THE NEW UNLOCKED CRYSTAL HIDDEN NUMBER TOGETHER FOR SU
+        console.log(sumOfCrystalsNum)
 
+    //CONDITIONALS OF THE FUNCTION CALLED=========================================================
+    if(sumOfCrystalsNum > game_RandomNum) {  //IF THE SUM OF ALL CLICKED CRYSTAL NUMBERS ARE GREATER THAN THE GAME RANDOM NUMBER .....
+        losses++; //INCREMENT THE LOSE-COUNTER
+        alert("YOU LOSE!");
+        $(".lose-counter").html(losses)   //...ADD TO LOSE-COUNTER IN THE INDEX.HTML FILE
 
-    if(unlockedNum > game_RandomNum) {  //IF THE SUM OF ALL CLICKED CRYSTAL NUMBERS ARE GREATER THAN THE GAME RANDOM NUMBER .....
-        lost--;
-        $("lose-counter").html(win)
-        console.log("you lose")
-        //alert("YOU LOSE!");
+        startGame(); //RESETS THE GAME AGAIN AFTER LOST
+
     }
-    else if(unlockedNum === game_RandomNum) {   //BUT IF THE SUM OF ALL CLICKED CRYSTAL NUMBERS EQUAL THE GAME RANDOM NUMBER...
-        win++;
-        $(".win-counter").html(win)
-        console.log("you win")
-        //alert("YOU WIN!");
+    else if(sumOfCrystalsNum === game_RandomNum) {   //BUT IF THE SUM OF ALL CLICKED CRYSTAL NUMBERS EQUAL THE GAME RANDOM NUMBER...
+        wins++;  //INCREMENT THE WIN-COUNTER
+        alert("YOU WIN!");
+        $(".win-counter").html(wins) //...ADD TO WIN COUNTER IN THE INDEX.HTML FILE
+
+        startGame(); //RESETS THE GAME AGAIN AFTER WIN
+
     }
 
-    //console.log(unlockedNum);
 });
 
 
